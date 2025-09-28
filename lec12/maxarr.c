bcd60ab4ae3f48ceb9dcdb7237cc3c23684aa7f5
KrishnaAsati16 <krishnaasati2006@gmail.com>
2025-09-28 12:34:15 +0530

    Add files via upload

---
+++ lec12/maxarr.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int arr[5]={3,12,52,34,5};
+    int max=arr[0];
+    for(int i=0;i<5;i++){
+        if(arr[i]>max){
+        max= arr[i];
+        }
+    
+}
+printf("the max of array is %d",max);
+    return 0;
+}
---
+++ lec12/searchingarr.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int arr[5]={3,12,52,34,5};
+    int target=
+    for( i = 0; i < count; i++){
+    }
+    (int i=0;i<5;i++){
+        if(arr[i]==target){
+        printf("%d",arr[i]);
+          break;
+        }
+        else{
+            printf("not found");
+        
+        }
+    
+    }
+}
